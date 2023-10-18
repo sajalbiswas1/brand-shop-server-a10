18f35953ea6ffb5d8c666cd892687dd31db80493
Programing Hero <sajalb1899@gmail.com>
2023-10-19 01:16:02 +0600

    click on cllint sile & delete on mycard data

--- index.js
+++ index.js
@@ -87,6 +87,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/userCard/remove/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCardCollection.deleteOne(query)
+            res.send(result)
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
